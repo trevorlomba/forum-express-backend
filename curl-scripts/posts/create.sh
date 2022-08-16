a2fef70f5e6c1d5bca1e6022379ea429786e26e9
Trevor Lomba <76894056+trevorlomba@users.noreply.github.com>
2022-08-16 00:36:29 -0400

    initial routes, models, curlsripts

---
+++ curl-scripts/posts/create.sh
@@ -0,0 +1,16 @@
+API="http://localhost:4741"
+URL_PATH="/posts"
+
+curl "${API}${URL_PATH}" \
+  --include \
+  --request POST \
+  --header "Content-Type: application/json" \
+  --header "Authorization: Bearer ${TOKEN}" \
+  --data '{
+    "post": {
+      "text": "'"${TEXT}"'",
+      "title": "'"${TITLE}"'"
+    }
+  }'
+
+echo
